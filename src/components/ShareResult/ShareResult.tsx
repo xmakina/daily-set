fa648440ed72c1c0a35129a0de91ea0b662ce65e
Alex Addams <alex.addams@coop.co.uk>
2026-03-25 21:34:09 +0000

    drop navigator.share, not even wordle uses it

--- src/components/ShareResult/ShareResult.tsx
+++ src/components/ShareResult/ShareResult.tsx
@@ -10,25 +10,16 @@ type Props = {
 
 const ShareResult = ({ guesses, time, duration }: Props) => {
   const [copied, setCopied] = useState(false);
-  const [shared, setShared] = useState(false);
 
   const date = new Intl.DateTimeFormat(undefined, {
     dateStyle: "medium",
   }).format(time);
 
-  const shareData = {
-    url: window.location.href,
-    text: `I solved the Daily Set for ${date} in ${format(duration)} and ${guesses} guesses. Can you do better? ${window.location.href}`,
-  };
-
   const copyText = () => {
-    if (navigator["canShare"] && navigator.canShare(shareData)) {
-      navigator.share(shareData);
-      setShared(true);
-    } else {
-      navigator.clipboard.writeText(shareData.text);
-      setCopied(true);
-    }
+    navigator.clipboard.writeText(
+      `I solved the Daily Set for ${date} in ${format(duration)} and ${guesses} guesses. Can you do better? ${window.location.href}`,
+    );
+    setCopied(true);
   };
 
   return (
@@ -36,14 +27,13 @@ const ShareResult = ({ guesses, time, duration }: Props) => {
       <div>
         You solved the Daily Set in {format(duration)} and {guesses} guesses!
       </div>
-      {navigator["share"] && <div></div>}
       {navigator["clipboard"] && (
         <div>
           <Button
             className="bg-green-700 hover:bg-green-500 text-white"
             onClick={copyText}
           >
-            {!copied && !shared && "Tell your friends!"}
+            {!copied && "Tell your friends!"}
             {copied && "Copied to your clipboard"}
           </Button>
         </div>
